23c3940e13ec4ff3e5ac216cf0ba3cb03db57e0a
Margaret Chapman <margaretchapman@Margarets-MacBook-Pro-5.local>
2015-02-26 09:25:00 -0500

    Implemented init() Javascript function to render images on canvas. Added a README.

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,10 @@
-// Replace this with your code...
+function init() {
+	var game_board = document.getElementById('game_canvas').getContext('2d');
+	var pac_img = new Image();
+	pac_img.onload = function() {
+		game_board.drawImage(pac_img, 320, 0, 465, 138, 0, 0, 800, 240);
+		game_board.drawImage(pac_img, 80, 40, 20, 20, 313, 134, 30, 30);
+	}
+	pac_img.src = 'pacman10-hp-sprite.png';
+
+}
