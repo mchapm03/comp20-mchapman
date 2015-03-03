eaddc29e3faf979b8035ecb9d6aa368aa7396293
Margaret Chapman <margaretchapman@resnet153-229.medford.tufts.edu>
2015-03-03 15:59:48 -0500

    Trying out XMLHttpRequest

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,8 @@
-// Your JavaScript code here...
+function parse() {
+	request = new XMLHttpRequest();
+	request.open("GET", "data.json", true);
+
+	request.sent();
+	console.log(request.responseHtml);
+
+}
